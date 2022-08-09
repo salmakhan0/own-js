90d3115a53553b031fc19fe7e248941c7c3380e9
Salma Khan <74590990+salmakhan0@users.noreply.github.com>
2022-08-10 00:44:25 +0600

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,24 @@
+var litter = prompt("Enter The Litter:");
+
+switch (litter) {
+    case "u":
+        console.log("Voiwl");
+        break;
+
+    case "o":
+        console.log("Voiwl");
+        break;
+
+    case "i":
+        console.log("Voiwl");
+        break;        
+    case "e":
+        console.log("Voiwl");
+        break;
+        case "a":
+            console.log("Voiwl");
+            break;        
+    default:
+        console.log("Consonant");
+    
+}
