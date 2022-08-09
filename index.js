dd7af69943badc8aea78b6447014b9747b6feee0
Salma Khan <74590990+salmakhan0@users.noreply.github.com>
2022-08-09 21:28:30 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+var num= prompt ("Entry the number: ");
+
+if(num>=80)
+console.log("A+");
+
+else if(num>=70)
+console.log("A");
+
+else if(num>=60)
+console.log("A-");
+
+else if(num>=50)
+console.log("B");
+
+else if(num>=40)
+console.log("C");
+
+else if(num>=33)
+console.log("D");
+
+else 
+console.log("F");
