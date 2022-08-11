9e110058bfae9667fc57bdceb00d75d3f96ee7bb
Salma Khan <74590990+salmakhan0@users.noreply.github.com>
2022-08-12 00:01:57 +0600

    Add files via upload

---
+++ index3.js
@@ -0,0 +1,24 @@
+function add(x,y){
+    var result = x+y ;
+    document.write("Sum =" + result + "<br>");
+}
+
+function sub(x,y){
+    var result = x+y ;
+    document.write("Sub  =" + result + "<br>");
+}
+
+function mul(x,y){
+    var result = x+y ;
+    document.write("Mul =" + result + "<br>");
+}
+
+function div(x,y){
+    var result = x+y ;
+    document.write("Div =" + result + "<br>");
+}
+
+add(10,10);
+sub(10,10);
+mul(10,10);
+div(10,10);
